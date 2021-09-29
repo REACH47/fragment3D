3415fe9580ef23efd75e5c53e910de1b6f5395d1
steveeum <steveeum@gmail.com>
2021-09-29 01:02:46 -0400

    create-react-app and Initialized project. Created folder structures and placeholders

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./Components/Header/Header";
+import Gallery from "./Components/Gallery/Gallery";
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Gallery />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Gallery/Gallery.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Gallery.scss";
+
+function Gallery() {
+  return (
+    <div>
+      <h1 className="gallery">Gallery Component</h1>
+    </div>
+  );
+}
+
+export default Gallery;
