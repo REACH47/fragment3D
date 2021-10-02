b89cd8423b1d3f6b63ebdd8e3fe0c88546a13af3
steveeum <steveeum@gmail.com>
2021-10-02 00:26:28 -0400

    Renamed branch to be more descriptive

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Header from "./Components/Header/Header";
+import Gallery from "./Components/Gallery/Gallery";
+import UploadModal from "./Components/UploadModal/UploadModal";
+import UploadButton from "./Components/UploadButton/UploadButton";
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <Header title="Fragm3ntD" />
+      <Gallery />
+      <UploadButton />
+    </div>
+  );
+}
+
+export default App;
