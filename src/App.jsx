543223ab278a3d46b3e7bfaa4c785c3a0a54ca7d
steveeum <steveeum@gmail.com>
2021-10-03 02:41:43 -0400

    Removed unused components from App.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import Header from "./Components/Header/Header";
 import Gallery from "./Components/Gallery/Gallery";
-import UploadModal from "./Components/UploadModal/UploadModal";
-import UploadButton from "./Components/UploadButton/UploadButton";
 import "./App.scss";
 
 function App() {
@@ -9,7 +7,6 @@ function App() {
     <div className="App">
       <Header title="Fragm3ntD" />
       <Gallery />
-      <UploadButton />
     </div>
   );
 }
