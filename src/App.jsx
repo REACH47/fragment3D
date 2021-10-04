5673732449bc0aade8da9658fd468eddc72c0564
Charles Lu <kuanminglu0529@gmail.com>
2021-10-03 17:23:42 -0700

    FRG3D-7 created scss variable for reusability. Created input form for upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Header from "./Components/Header/Header";
 import Gallery from "./Components/Gallery/Gallery";
 import "./App.scss";
+import UploadForm from "./Components/UploadForm/UploadForm";
 
 function App() {
   return (
     <div className="App">
       <Header title="Fragm3ntD" />
       <Gallery />
+      <UploadForm />
     </div>
   );
 }
--- src/Components/UploadForm/UploadForm.jsx
+++ src/Components/UploadForm/UploadForm.jsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useState } from "react";
+import "./UploadForm.scss";
 
 function UploadForm() {
+    const [file, setfile] = useState(null);
+
+    const saveFile = () => {
+
+    }
+
     return (
-        <div>
-            
-        </div>
-    )
+        <form action="" className="upload-form" onSubmit={saveFile}>
+            <h2 htmlFor="">Upload 3D model</h2>
+            <label htmlFor="fileInput" className="label__file-input">Select a .obj file to upload</label>
+            <input type="file" />
+            <label htmlFor="modelTitle" className="label__file-title">3D Model title</label>
+            <input type="text" />
+            <input
+                type="submit"
+                value="Upload"
+                className="upload-form__submit-button"
+            />
+        </form>
+    );
 }
 
-export default UploadForm
+export default UploadForm;
