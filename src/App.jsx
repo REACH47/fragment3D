d82748c934eaa2b2a5cea19eb87fe0a4945b6f03
Charles Lu <kuanminglu0529@gmail.com>
2021-10-06 21:46:13 -0700

    FRG3D-7 removed unused package and import

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import Header from "./Components/Header/Header";
 import Gallery from "./Components/Gallery/Gallery";
 import UploadButton from "./Components/UploadButton/UploadButton";
 import "./App.scss";
-import UploadForm from "./Components/UploadForm/UploadForm";
 
 function App() {
   return (
